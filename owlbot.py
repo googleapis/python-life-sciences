7f38caadc7ee95b963161c06b0d2dc66759d5ca4
Dan Lee <dandhlee@google.com>
2021-08-30 19:50:33 +0000

    chore: update owlbot

--- owlbot.py
+++ owlbot.py
@@ -39,3 +39,26 @@ s.move(templated_files, excludes=[".coveragerc"]) # the microgenerator has a goo
 # ----------------------------------------------------------------------------
 
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)
+
+# ----------------------------------------------------------------------------
+# Main Branch migration
+# ----------------------------------------------------------------------------
+
+s.replace(
+  "*.rst",
+  "master",
+  "main"
+)
+
+s.replace(
+  "CONTRIBUTING.rst",
+  "kubernetes/community/blob/main",
+  "kubernetes/community/blob/master"
+)
+
+s.replace(
+  ".kokoro/*",
+  "master",
+  "main"
+)
+
